089c276c3890b9bb7448d3ed044eeb3c31e53289
Viktoria Navasardyan <88617144+ViktoryaNavasardyan@users.noreply.github.com>
2021-09-14 01:54:57 +0400

    Add files via upload

---
+++ InvalidParameterException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamic_Polymorphism.Model
+{
+    public class InvalidParameterException : Exception
+    {
+        public InvalidParameterException() { }
+        public InvalidParameterException(string message) : base(message) { }
+        public InvalidParameterException(string message, Exception inner) : base(message, inner) { }
+        protected InvalidParameterException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+
+    }
+    
+    
+}
---
+++ NotSupportedInterfaceException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamic_Polymorphism.Model
+{
+    public class NotSupportedInterfaceException : Exception
+    {
+        public NotSupportedInterfaceException() { }
+        public NotSupportedInterfaceException(string message) : base(message) { }
+        public NotSupportedInterfaceException(string message, Exception inner) : base(message, inner) { }
+        protected NotSupportedInterfaceException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
