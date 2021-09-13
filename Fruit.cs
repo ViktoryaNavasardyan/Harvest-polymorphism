a5e8696fa74316f19c455db84bb8f529911bbaae
Viktoria Navasardyan <88617144+ViktoryaNavasardyan@users.noreply.github.com>
2021-09-14 01:51:41 +0400

    Update Fruit.cs

--- Fruit.cs
+++ Fruit.cs
@@ -6,31 +6,23 @@ using System.Threading.Tasks;
 
 namespace Dynamic_Polymorphism.Model
 {
-    class Fruit : Harvest, IDried
+    class Fruit : Harvest
     {
-        public Fruit ( string name, double weight, string metric, string taste) : base(name, weight, metric, taste){ }
-        public Fruit(string name, double weight, string metric) : base(name, weight, metric, null) { }
-        public override void ToRipe()
+        public Fruit ( string name, double weight, Metric metric, Taste taste = Taste.sour, State state = State.growing ) : base(name, weight, metric, taste , state){ }
+        public override void Ripe()
         {
+            base.Ripe();
             _weight += 2 * _weight + 1;
-            Console.WriteLine("The fruit has riped.");
+            _taste = Taste.sweet;
         }
-        public override void Place()
-        {
-            Console.WriteLine("On the trees.");
-        }
-        public void Dried()
-        {
-            _weight -= _weight / 2;
-        }
-        public void ChangeName(string newName)
-        {
-            _name = newName;
-        }
+        public override void Marinate() => throw new NotSupportedInterfaceException();
+        public override void Location() => Console.WriteLine("on the trees.");
+
         public override void Show()
         {
-            Console.Write($"Fruit name = {_name},  weight = {_weight} {_metric},  taste = {_taste}, I am growing ");
-            Place();
+            Console.Write($"Fruit name = {_name},  weight = {_weight} {_metric}, state = {_state}, taste = {_taste}, I am growing ");
+            Location();
         }
+
     }
 }
