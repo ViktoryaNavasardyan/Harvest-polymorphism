20b6fda98ad1a49aa20a57a9b0070446e327dcb3
Viktoria Navasardyan <88617144+ViktoryaNavasardyan@users.noreply.github.com>
2021-09-13 10:56:28 +0400

    Add files via upload

---
+++ Fruit.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamic_Polymorphism.Model
+{
+    class Fruit : Harvest
+    {
+        string _name;
+        string Taste
+        {
+            get => _taste;
+            set => _taste = value;
+        }
+        public Fruit (double weight, string name, string taste) : base(weight)
+        {
+            _name = name;
+            Taste = taste;
+        }
+        public override void ToRipe()
+        {
+            _weight += 2 * _weight + 1;
+            Console.WriteLine("The fruit has riped.");
+        }
+        public override void Place()
+        {
+            Console.WriteLine("On the trees.");
+        }
+    }
+}
---
+++ Harvest․cs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamic_Polymorphism.Model
+{
+    abstract class Harvest
+    {
+        protected double _weight;
+        protected string _taste; //qaxcr, ttu, dary
+        public Harvest (double w)
+        {
+            _weight = w;
+        }
+        public abstract void ToRipe();
+        public virtual void Place()
+        {
+            Console.WriteLine("From mainland.");
+        }
+    }
+}
