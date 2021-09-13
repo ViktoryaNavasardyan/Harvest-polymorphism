2a526096929c1060e2c55b4a090adbe9075758a4
Viktoria Navasardyan <88617144+ViktoryaNavasardyan@users.noreply.github.com>
2021-09-14 01:53:48 +0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Dynamic_Polymorphism.Model;
 using System.Collections.Generic;
 
@@ -9,41 +9,79 @@ namespace Dynamic_Polymorphism
     {
         static void Main(string[] args)
         {
-            var harvests = new List<Harvest> { new Fruit("apple", 2000, "kg", "sweet"), new Vegetable("tomatoes", 5.5, "kg", "normal"), new Berry("strawberry", 500, "g", "sweet and sour") };
-            foreach (var harvest in harvests)
-            {
-                harvest.CheckMetric();
-                harvest.Show();
-                Console.WriteLine();
-            }
-            Console.WriteLine();
-            Console.WriteLine("--------------Riping------------");
-            Console.WriteLine();
-            foreach (var harvest in harvests)
-            {
-                harvest.ToRipe();
-                harvest.CheckMetric();
-                harvest.Show();
-                Console.WriteLine();
-            }
-            //Use IDried interface for fruits.
-            Console.WriteLine("______________________Drying___________________________");
-            Fruit fruit = new Fruit("grapes", 20, "kg");
-            fruit.Show();
-            fruit.Dried();
-            fruit.ChangeName("raisins");
-            fruit.Show();
-            //Use IMarinate interface for vegetables.
-            Console.WriteLine("_____________________Marinate__________________________");
-            Vegetable vegetable = new Vegetable("cucumber", 3000, "g");
-            vegetable.CheckMetric();
-            vegetable.Show();
-            vegetable.Marinate();
-            vegetable.ChangeName("pickles");
-            vegetable.Show();
+            //creating harvests and checking Dry and Marinate methods
+            CreatingHarvests();
 
+            //checking exceptions
+            ChechkingExceptions();
 
             Console.ReadLine();
+        }
+
+        static void CreatingHarvests()
+        {
+            //creating harvests
+            var harvests = new List<Harvest>()
+            {
+                new Fruit("Apples", 200, Metric.kg),
+                new Fruit("Grapes", 20, Metric.kg, Taste.sweet, State.riped),
+                new Vegetable ("Tomatoes", 5, Metric.kg, Taste.unknown, State.unknown),
+                new Vegetable ("Cucumbers", 10, Metric.kg),
+                new Berry("Raspberrys", 500, Metric.g, Taste.sweet),
+                new Berry("Strawberryս", 700, Metric.g)
+            };
+            Console.WriteLine("All harvest---->");
+            foreach (var item in harvests)
+            {
+                item.Show();
+            }
+            Console.WriteLine();
+
+            //checking Dry method
+            harvests[0].Dry();
+            harvests[1].Dry();
+            Console.WriteLine("Dried fruits:");
+            harvests[0].Show();
+            harvests[1].Show();
+            Console.WriteLine();
+            //checking Marinate method
+            harvests[2].Marinate();
+            harvests[3].Marinate();
+            Console.WriteLine("Marinate vegetables:");
+            harvests[2].Show();
+            harvests[3].Show();
+            Console.WriteLine();
+            //checking Rise method
+            Console.WriteLine("Rised harvests:");
+            foreach(var harvest in harvests)
+            {
+                harvest.Ripe();
+                harvest.Show();
+            }
+            Console.WriteLine();
+        }
+        static void ChechkingExceptions()
+        {
+            Console.WriteLine("_________________________________Exeptions______________________________________________");
+            try
+            {
+                Harvest harvest2 = new Fruit("Banana", -2, Metric.piece);
+                harvest2.Show();
+            }
+            catch (InvalidParameterException)
+            {
+                Console.WriteLine("Invalid parametr.");
+            }
+            try
+            {
+                Harvest harvest3 = new Vegetable("Onion", 5, Metric.kg);
+                harvest3.Show();
+                harvest3.Dry();
+            }
+            catch (NotSupportedInterfaceException)
+            {
+                Console.WriteLine("This operation was not supported.");
+            }
         }
     }
 }
