3763397cf465ab2557dfd7f0ad9d587121ec72ad
Viktoria Navasardyan <88617144+ViktoryaNavasardyan@users.noreply.github.com>
2021-09-14 01:51:05 +0400

    Update Berry.cs

--- Berry.cs
+++ Berry.cs
@@ -8,20 +8,20 @@ namespace Dynamic_Polymorphism.Model
 {
     class Berry : Harvest
     {
-        public Berry(string name, double weight, string metric, string taste) : base(name, weight, metric, taste){ }
-        public override void ToRipe()
+        public Berry(string name, double weight, Metric metric, Taste taste = Taste.sour, State state = State.growing) : base(name, weight, metric, taste, state) { }
+        public override void Ripe()
         {
-            _weight += 1.5 * _weight + 1;
-            Console.WriteLine("The berry has riped.");
+            base.Ripe();
+            _taste = Taste.sweet;
+            _weight += (1.5 * _weight) + 1;
         }
-        public override void Place()
-        {
-            Console.WriteLine("On the bush");
-        }
+        public override void Marinate() => throw new NotSupportedInterfaceException();
+        public override void Dry() => throw new NotSupportedInterfaceException();
+        public override void Location() => Console.WriteLine("on the bush.");
         public override void Show()
         {
-            Console.Write($"Berry name = {_name},  weight = {_weight} {_metric},  taste = {_taste}, I am growing ");
-            Place();
+            Console.Write($"Berry name = {_name},  weight = {_weight} {_metric}, state = {_state}, taste = {_taste}, I am growing ");
+            Location();
         }
     }
 }
