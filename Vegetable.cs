b964811d8177da6e3a3732d19ff3428beaead59b
Viktoria Navasardyan <88617144+ViktoryaNavasardyan@users.noreply.github.com>
2021-09-14 01:54:14 +0400

    Update Vegetable.cs

--- Vegetable.cs
+++ Vegetable.cs
@@ -6,32 +6,22 @@ using System.Threading.Tasks;
 
 namespace Dynamic_Polymorphism.Model
 {
-    class Vegetable : Harvest, IMarinate
+    class Vegetable : Harvest
     {
-        public Vegetable(string name, double weight, string metric, string taste) : base(name, weight, metric, taste) { }
+        public Vegetable(string name, double weight, Metric metric, Taste taste = Taste.unknown, State state = State.growing) : base(name, weight, metric, taste, state) { }
 
-        public Vegetable(string name, double weight, string metric) : base(name, weight, metric, null) { }
-        public override void ToRipe()
+        public override void Ripe()
         {
+            base.Ripe();
             _weight += 2 * _weight + 5;
-            Console.WriteLine("The vegetable has riped.");
         }
-        public override void Place()
-        {
-            Console.WriteLine("In the ground.");
-        }
-        public void Marinate()
-        {
-            _taste = "sour";
-        }
-        public void ChangeName(string newName)
-        {
-            _name = newName;
-        }
+        public override void Dry() => throw new NotSupportedInterfaceException();
+        public override void Location() => Console.WriteLine("in the ground.");
+
         public override void Show()
         {
-            Console.Write($"Vegetable name = {_name},  weight = {_weight} {_metric},  taste = {_taste}, I am growing ");
-            Place();
+            Console.Write($"Vegetable name = {_name},  weight = {_weight} {_metric}, state = {_state}, taste = {_taste}, I am growing ");
+            Location();
         }
         
     }
